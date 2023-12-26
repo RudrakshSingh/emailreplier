f4da8035e75c5e53c7c5534ff988340d5b71bd21
RudrakshSingh <100840928+RudrakshSingh@users.noreply.github.com>
2023-12-26 21:45:45 +0530

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,7 +1,7 @@
 const CLIENT_ID =
-  "1081102446726-cr1a243tcn134ca8illdh3ng0qhp0g4e.apps.googleusercontent.com";
-const CLEINT_SECRET = "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";
+  "1081102446726-cr1a243tcn134ca8illdh3ng0qhe.apps.googleusercontent.com";
+const CLEINT_SECRET = "GOCSPX-72dmPh5CZ1VQzUl9R4lst5";
 const REDIRECT_URI = "https://developers.google.com/oauthplayground";
 const REFRESH_TOKEN =
-  "1//04OnRsOXSU9UxCgYIARAAGAQSNwF-L9Ir5NSlGpP87fUWEZ-Isyhj-T3yc-S-IkMp-ZCwB8CuH5rndFoh39xsD5Qx3WeH-858G60";
+  "1//04OnRsOXSU9UxCgYIARAAGAQSNwF-L9Ir5NSlGpP87fUWEZ-ZCwB8CuH5rndFoh39xsD5Qx3WeH-858G60";
 module.exports = { CLIENT_ID, CLEINT_SECRET, REDIRECT_URI, REFRESH_TOKEN };
